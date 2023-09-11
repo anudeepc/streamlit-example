a020f9088faee5c97c607ac9397bbad38d4a9501
anudeepc <anudeepchikyala@gmail.com>
2023-09-11 16:51:11 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,33 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+import random
 
-"""
-# Welcome to Streamlit!
+def generate_question():
+    num1 = random.randint(2, 9)
+    num2 = random.randint(2, 9)
+    return num1, num2
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+def main():
+    correct_answers = 0
+    total_questions = 10  # You can change this to have more or fewer questions
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    for i in range(total_questions):
+        num1, num2 = generate_question()
+        print(f"Question {i + 1}: What is {num1} * {num2}?")
+        
+        user_answer = input("Your answer: ")
+        
+        try:
+            user_answer = int(user_answer)
+        except ValueError:
+            print("Please enter a valid integer.")
+            continue
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+        if user_answer == num1 * num2:
+            print("Correct!")
+            correct_answers += 1
+        else:
+            print(f"Wrong. The correct answer is {num1 * num2}.")
 
+    print(f"You got {correct_answers} out of {total_questions} questions correct.")
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
